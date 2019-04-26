82139d073a5a1d4b408eeae4a047062abf22b417
Nataly09 <natali.strashko@gmail.com>
2019-04-26 19:17:42 +0300

    homework

---
+++ homework6/script.js
@@ -0,0 +1,34 @@
+"use strict";
+console.log('№1');
+
+var arr = [2, -5, 1, -2, 0, -3, 2, -14];
+var posArr = arr.filter(function(number) {
+    return number >= 0;
+  });
+console.log(posArr);
+  var rootArr = posArr.map(function(name){
+           return (Math.sqrt(name));
+  });
+  console.log(rootArr);
+
+  console.log('№2');
+
+  var arr1 = [1, 1, 1, 1, 1, 0];
+  var zero_ = false;
+
+var result = arr1.reduce(function(sum, current, index, array){
+    return array.indexOf(0) == -1 || index < array.indexOf(0) ? sum + current : sum;
+});
+
+console.log(result);
+
+console.log('№3');
+function summArguments(...args){
+    return args.reduce(function(sum, current){
+        return sum + current;
+    });
+};
+
+console.log(summArguments(1, 2, 4, 4, 5));
+
+    
